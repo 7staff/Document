78548bfa30fa1f1e306c0b8d3cc9f5adfbac6ec4
7staff <42589270+7staff@users.noreply.github.com>
2019-02-11 20:50:38 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace DocumentApplication
+{
+    class Document
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Document");
+                Console.WriteLine("");
+                Console.WriteLine("Document Name:");
+
+                string DocFile = Console.ReadLine();
+
+                Console.WriteLine("Document Content:");
+
+                string character = Console.ReadLine();
+                string filename = DocFile + ".txt";
+                string savedata = Environment.CurrentDirectory  + filename;
+                if (!File.Exists(savedata))
+
+                {
+
+                    File.WriteAllText(savedata, character);
+
+                }
+
+                Console.WriteLine(filename + " was successfully saved. The document contains " + character.Length + " characters");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+
+        }
+
+    }
+
+}
